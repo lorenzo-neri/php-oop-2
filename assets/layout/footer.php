b4885409b6afadd400f0fad37ed1af67255f9d71
lorenzo-neri <lorenzo21neri@gmail.com>
2023-10-20 14:42:35 +0200

    Initial commit

---
+++ assets/layout/footer.php
@@ -0,0 +1,10 @@
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
+<!-- Development only cdn, disable in production -->
+
+<!-- <script src='https://unpkg.com/vue@3/dist/vue.global.js'></script> -->
+<!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.5.1/axios.min.js" integrity="sha512-emSwuKiMyYedRwflbZB2ghzX8Cw8fmNVgZ6yQNNXXagFzFOaQmbvQ1vmDkddHjm5AITcBIZfC7k4ShQSjgPAmQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script> -->
+<!-- <script src='./assets/js/main.js'></script> -->
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+/* 
+Immaginare quali sono le classi necessarie per creare uno shop online con le seguenti caratteristiche:
+- L'e-commerce vende prodotti per animali.
+- I prodotti sono categorizzati, le categorie sono Cani o Gatti.
+- I prodotti saranno oltre al cibo, anche giochi, cucce, etc.
+
+Stampiamo delle card contenenti i dettagli dei prodotti, come immagine, titolo, prezzo, icona della categoria ed il tipo di articolo che si sta visualizzando (prodotto, cibo, gioco, cuccia).
+
+Bonus (non opzionale):
+organizzate il progetto come visto stamattina a lezione usando varie sottocartelle per inserire classi, layout e dati.
+*/
+
+include __DIR__ . '/assets/layout/head.php';
+
+?>
+
+<h1>HELLO WORLD!</h1>
+
+
+<?php
+#FOOTER
+include __DIR__ . '/assets/layout/footer.php'
+?>
